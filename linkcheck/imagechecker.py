54c1b4980234fa7e77806a76077f46d2c7ee68c2
adamrmor <adamrmor@users.noreply.github.com>
2017-08-09 10:39:54 +1200

    Update imagechecker.py

--- linkcheck/imagechecker.py
+++ linkcheck/imagechecker.py
@@ -6,18 +6,19 @@ import json
 # list.csv should contain Auckland Museum API image links without rendering information
 # e.g. http://api.aucklandmuseum.com/id/media/v/320810
 
-with open('list.csv', "r") as list:
+with open('List2.csv', "r") as list:
     l = csv.reader(list)
     for row in l:
-        row = ''.join(row)
-        row = row+str("/metadata")
-        headers = {'accept': "application/json"}
-        response = requests.request("GET", row, headers=headers)
-        res = [''.join(row),response]
-        if "404" in str(res):
-            with open("errors.csv","ab") as outfile:
-                writer = csv.writer(outfile)
-                writer.writerow(res)
+        row = ''.join(row)+str("/metadata")
 
+        # set headers, otherwise API sends image
+        headers = {'accept': "application/json"}
+        response = requests.get(row, headers=headers)
+
+        if response.status_code == 404:
+            with open("errorsTEST.csv","ab") as outfile:
+                writer = csv.writer(outfile)
+                writer.writerow([row])
+            print "Added 1."
 
 list.close()
